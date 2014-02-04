64ec898bb78903b6df6953bc6e650365df809ef1
Martin Kobetic <mkobetic@gmail.com>
2014-02-03 21:41:37 -0500

    reduce templates

--- stampede.go
+++ stampede.go
@@ -11,7 +11,7 @@ import (
 var (
 	root      = flag.String("root", "$HOME/mail", "directory containing the mail archive")
 	Root      []*MailDirectory
-	Templates = template.Must(template.New("all").ParseGlob("templates/*"))
+	Templates = template.Must(template.New("all").ParseGlob("*.template"))
 )
 
 func main() {
@@ -21,9 +21,10 @@ func main() {
 	for _, t := range Templates.Templates() {
 		log.Println(t.Name())
 	}
-	http.HandleFunc("/", render("stampede"))
-	http.HandleFunc("/folder", render("folder"))
-	http.HandleFunc("/message", render("message"))
+	http.HandleFunc("/", render("stampede.template"))
+	http.HandleFunc("/folder", query("folder"))
+	http.HandleFunc("/message", query("message"))
+	http.Handle("/js", http.FileServer(http.Dir("./js")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -31,6 +32,16 @@ func render(template string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		err := Templates.ExecuteTemplate(w, template, Root)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func query(s string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		var err error
 		if err != nil {
 			log.Fatal(err)
 		}
